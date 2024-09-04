a7a173253d3206d6fcdad4eeaa5e5568b03ef6f5
Rizki Januar I. <id.rizkijanuar@gmail.com>
2024-09-05 01:11:31 +0700

    🔥 Initial commit

---
+++ src/data/experience.ts
@@ -0,0 +1,46 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "Februari 2023 - Februari 2024",
+    title: "Magang | Kontrak | Developer",
+    company: "Arkana Technology Indonesia",
+    companyUrl: "https://www.arkanatechnology.com/",
+    description:
+      "Saya memulai di Arkana di bulan Februari 2023 sebagai Mahasiswa Magang kemudian menjadi Pegawai Kontrak, disana saya melakukan banyak hal seperti membuat Desain UI / UX, membuat landing page, melakukan sedikit perbaikan fitur, membuat dokuemntasi teknis, membuat aplikasi android dan membantu pada bagian customer service. Banyak hal saya belajar disini, terimakasih untuk segenap Tim Arkana!",
+    technologies: [
+      "UI / UX",
+      "Next JS",
+      "Tailwind",
+      "PHP Yii2",
+      "Notion",
+      "Super.so",
+      "React Native",
+    ],
+    logo: "/logos/arkana.jpg",
+  },
+  {
+    dates: "Sepetember 2023 — Desember 2023",
+    title: "Kampus Merdeka | Android Engineer",
+    company: "PT. PRESTASI GEMILANG INDONESIA",
+    companyUrl: "https://indonesiagemilang.com",
+    description:
+      "Program ini dinaungi oleh Kampus Merdeka dengan bermitra perusahaan Prestasi Gemilang Indonesia. Saya memulai disini sebagai Android Engineer, bertugas membuat aplikasi kalkulator, melakukan slicing halaman dari desain ke kode, menggunakan library seperti halnya ViewBinding pengganti dari findById, kemudian menerima data menggunakan RecyclerView, Firebase, Retrofit. Kemudian membuat aplikasi android Movie dan membuat aplikasi android ChiliChare yang berfokus pada AgriCulture.",
+    technologies: [
+      "Kotlin",
+      "RecyclerView",
+      "Retrofit",
+      "Coroutine",
+      "Dagger",
+      "Epoxy",
+      "Glide",
+      "Youtube SDK",
+      "Firebase",
+      "DataBinding",
+      "GMS Play Service",
+      "Logging Interceptor",
+      "Unit Testing",
+    ],
+    logo: "/logos/the-president.jpg",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,15 @@
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/rizkijanuarr",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://linkedin.com/in/rzkjanuarr",
+    icon: IconLinkedin,
+  },
+];
